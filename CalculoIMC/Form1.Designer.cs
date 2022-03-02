d3c058cc07f02e942c196e29ec6d9c35016e6235
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2022-03-02 13:54:38 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoIMC/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CalculoIMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.label1 = new System.Windows.Forms.Label();
+            this.pesoText = new System.Windows.Forms.TextBox();
+            this.alturaText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.calcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(141, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cálculo do IMC";
+            // 
+            // pesoText
+            // 
+            this.pesoText.Location = new System.Drawing.Point(138, 138);
+            this.pesoText.Name = "pesoText";
+            this.pesoText.Size = new System.Drawing.Size(38, 20);
+            this.pesoText.TabIndex = 1;
+            // 
+            // alturaText
+            // 
+            this.alturaText.Location = new System.Drawing.Point(244, 138);
+            this.alturaText.Name = "alturaText";
+            this.alturaText.Size = new System.Drawing.Size(38, 20);
+            this.alturaText.TabIndex = 2;
+            this.alturaText.TextChanged += new System.EventHandler(this.altura_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(119, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Peso (kg)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(228, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Altura (m)";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(159, 245);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(99, 27);
+            this.result.TabIndex = 5;
+            this.result.Text = "Resultado";
+            this.result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.result.UseCompatibleTextRendering = true;
+            // 
+            // calcular
+            // 
+            this.calcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcular.Location = new System.Drawing.Point(162, 185);
+            this.calcular.Name = "calcular";
+            this.calcular.Size = new System.Drawing.Size(96, 35);
+            this.calcular.TabIndex = 7;
+            this.calcular.Text = "Calcular";
+            this.calcular.UseVisualStyleBackColor = true;
+            this.calcular.Click += new System.EventHandler(this.calcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 335);
+            this.Controls.Add(this.calcular);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.alturaText);
+            this.Controls.Add(this.pesoText);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pesoText;
+        private System.Windows.Forms.TextBox alturaText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button calcular;
+        private System.Windows.Forms.Label result;
+    }
+}
+
---
+++ CalculoIMC/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoIMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+            calcular.Enabled = false;
+        }
+
+        private void altura_TextChanged(object sender, EventArgs e) {
+            calcular.Enabled = alturaText.Text != "" ? true : false;
+        }
+
+        private void calcular_Click(object sender, EventArgs e) {
+            double.TryParse(alturaText.Text, out double altura);
+            double.TryParse(pesoText.Text, out double peso);
+            if (altura == null) return;
+
+            double imc = peso / Math.Pow(altura, 2);
+
+            StringBuilder str = new StringBuilder($"Seu IMC é {imc:F2}");
+
+            MessageBox.Show(str.ToString());
+
+            result.Text = str.ToString();
+        }
+    }
+}
